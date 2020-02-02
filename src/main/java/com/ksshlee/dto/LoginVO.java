bd838d084f114197beea9aa85e6739602a708d04
ksshlee <ksshlee@naver.com>
2020-02-02 22:49:36 +0900

    add loginVO to save loginform

---
+++ src/main/java/com/ksshlee/dto/LoginVO.java
@@ -0,0 +1,27 @@
+package com.ksshlee.dto;
+
+public class LoginVO {
+	private String user_name;
+	private String id;
+	private String pwd;
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
